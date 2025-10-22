e45b8e983f3aaa3d9d5cd8214649726818d29af6
Altzitrone <dvu303005@gmail.com>
2025-10-22 21:34:34 +0700

    First commit - RPG Game Project

--- README.md
+++
@@ -1,2 +0,0 @@
-# GAME_JAVA
-A rpg game with JAVA language made by 5 people.
---
+++ core/src/main/java/oop/duong/rpggame/RPGGame.java
@@ -0,0 +1,11 @@
+package oop.duong.rpggame;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class RPGGame extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
