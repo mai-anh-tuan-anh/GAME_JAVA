a7b95635299c58b722658cb1189f09e9c62599fc
MATA <sktt10mtd@gmail.com>
2025-11-13 01:47:51 +0700

    Finished Map

---
+++ Raw Asset/Tiled_files/exterior.tsx
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="exterior" tilewidth="16" tileheight="16" tilecount="969" columns="17">
+ <image source="exterior.png" width="272" height="912"/>
+ <tile id="528">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="14" y="0" width="2" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="575">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="9" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="753">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10" y="3" width="6" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="754">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="755">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2" width="6" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="757">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="6" width="16" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="758">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8" width="5" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="770">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="0" width="13" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="772">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="11" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="773">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7" y="0" width="9" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="774">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="775">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="7" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="787">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="12" height="11"/>
+  </objectgroup>
+ </tile>
+ <tile id="788">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="789">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="791">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="8"/>
+  </objectgroup>
+ </tile>
+</tileset>
